89fafb6243a9886698fb9e3d04fb1d9fe6a66902
Mert Gedik <mert7a@hotmail.de>
2026-03-09 15:27:58 +0100

    Initial Commit

---
+++ PrayerTimesWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrayerTimesWidget
+//
+//  Created by Mert Gedik on 09.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrayerTimesWidget/PrayerTimesWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimesWidgetApp.swift
+//  PrayerTimesWidget
+//
+//  Created by Mert Gedik on 09.03.26.
+//
+
+import SwiftUI
+
+@main
+struct PrayerTimesWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
